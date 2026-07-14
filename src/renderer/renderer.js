ba92e2b2b526db5abc856b9f888f9b35b8325c28
TheShovel <niccata24@gmail.com>
2026-07-14 13:06:07 +0300

    fix small regex issue

--- src/renderer/renderer.js
+++ src/renderer/renderer.js
@@ -1608,8 +1608,12 @@ function stripEmojis(text) {
       /[\u{1F600}-\u{1F64F}\u{1F300}-\u{1F5FF}\u{1F680}-\u{1F6FF}\u{1F1E0}-\u{1F1FF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}\u{FE00}-\u{FE0F}\u{1F900}-\u{1F9FF}\u{1FA00}-\u{1FA6F}\u{1FA70}-\u{1FAFF}\u{200D}\u{2B50}\u{2934}\u{2935}\u{25AA}\u{25AB}\u{25FB}-\u{25FE}\u{25FC}\u{25FD}\u{2B1B}\u{2B1C}\u{2B55}]/gu,
       "",
     )
-    .replace(/(\*{1,2})\s+/g, "$1")
-    .replace(/\s+(\*{1,2})/g, "$1")
+    .replace(/\*\*([\s\S]*?)\*\*/g, function (_, content) {
+      return "**" + content.trim() + "**";
+    })
+    .replace(/(^|\s)\*([^*]+?)\*(\s|$)/g, function (_, pre, content, post) {
+      return pre + "*" + content.trim() + "*" + post;
+    })
     .replace(/ {2,}/g, " ");
 }
 
